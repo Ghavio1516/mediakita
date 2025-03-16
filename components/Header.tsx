294d5946b09454c57139560d145d50564b773830
Ghavio Rizky <ghavio1516@gmail.com>
2025-03-16 23:38:00 +0700

    add redirect when pressing home

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,24 @@
+import Link from 'next/link';
+
 export default function Header() {
-    return (
-      <header>
-        <div className="logo">
-          <img src="/mediakitalogo.png" alt="Media Kita Logo" />
-        </div>
-        <nav>
-          <a href="#" className="active">Home</a>
-          <a href="#">Hot News</a>
-          <a href="#">Entertainment</a>
-        </nav>
-      </header>
-    );
-  }
-  
+  return (
+    <header>
+      <div className="logo">
+        {/* Membungkus gambar logo dengan Link untuk mengarah ke halaman utama */}
+        <Link href="/">
+          <a>
+            <img src="/mediakitalogo.png" alt="Media Kita Logo" />
+          </a>
+        </Link>
+      </div>
+      <nav>
+        {/* Membungkus link "Home" dengan Link untuk navigasi */}
+        <Link href="/">
+          <a className="active">Home</a>
+        </Link>
+        <a href="#">Hot News</a>
+        <a href="#">Entertainment</a>
+      </nav>
+    </header>
+  );
+}
